20c1a90f794ea063c8981bc511ae02c74356f07e
Ivan Pokhabov <vanek3372@gmail.com>
2024-10-01 19:28:59 +0700

    docs: make docs for IMyTask

--- src/homework3/IMyTask.cs
+++ src/homework3/IMyTask.cs
@@ -1,10 +1,26 @@
 ﻿namespace homework3;
 
+/// <summary>
+/// Interface that represents a single operation and that executes async in different threads.
+/// </summary>
+/// <typeparam name="TResult">Operation result type.</typeparam>
 public interface IMyTask<TResult>
 {
+    /// <summary>
+    /// Gets a value indicating whether gets true if task completed.
+    /// </summary>
     public bool IsCompleted { get; }
 
+    /// <summary>
+    /// Gets result of task.
+    /// </summary>
     public TResult? Result { get; }
 
+    /// <summary>
+    /// Apply method to the result of this task.
+    /// </summary>
+    /// <param name="func">method to be applied to the result of current task.</param>
+    /// <typeparam name="TNewResult">Result type of <see cref="func"/>.</typeparam>
+    /// <returns>result of method <see cref="func"/> with current task result in argument.</returns>
     public IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult?, TNewResult> func);
 }
