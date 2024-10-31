b71f2e4795101c727a7473714242fa57767f69d4
Ivan Pokhabov <vanek3372@gmail.com>
2024-10-31 18:35:10 +0300

    fix: bugs

--- src/test1/CheckSumCalculator.cs
+++ src/test1/CheckSumCalculator.cs
@@ -11,11 +11,23 @@ public static class CheckSumCalculator
     private static readonly List<byte> Result = [];
 
     /// <summary>
+    /// Gets result of calculation.
+    /// </summary>
+    public static byte[] GetResult => [.. Result];
+
+    /// <summary>
+    /// Clear result of calculating.
+    /// </summary>
+    public static void Clear()
+    {
+        Result.Clear();
+    }
+
+    /// <summary>
     /// Calculate check-sum with 1 thread.
     /// </summary>
     /// <param name="path">Path to entry.</param>
-    /// <returns>MD5 hash.</returns>
-    public static byte[] Calculate(string path)
+    public static void Calculate(string path)
     {
         if (!Directory.Exists(path) && !File.Exists(path))
         {
@@ -27,30 +39,27 @@ public static class CheckSumCalculator
         {
             var entries = Directory.GetFileSystemEntries(path).Order();
             var directory = new DirectoryInfo(path);
-            var directoryNameHash = MD5.HashData(Encoding.UTF32.GetBytes(directory.Name));
+            var directoryNameHash = MD5.HashData(Encoding.UTF8.GetBytes(directory.Name));
             Result.AddRange(directoryNameHash);
 
             foreach (var entry in entries)
             {
-                Result.AddRange(Calculate(entry));
+                Calculate(entry);
             }
 
-            return [.. Result];
+            return;
         }
 
         using var fileContent = File.Open(path, FileMode.Open);
         var fileHash = MD5.HashData(fileContent);
         Result.AddRange(fileHash);
-
-        return [.. Result];
     }
 
     /// <summary>
     /// Calculate check-sum using multithreading.
     /// </summary>
     /// <param name="path">Path to entry.</param>
-    /// <returns>MD5 hash.</returns>
-    public static byte[] MultithreadingCalculate(string path)
+    public static void MultithreadingCalculate(string path)
     {
         if (!Directory.Exists(path) && !File.Exists(path))
         {
@@ -65,16 +74,13 @@ public static class CheckSumCalculator
             var directoryNameHash = MD5.HashData(Encoding.UTF32.GetBytes(directory.Name));
             Result.AddRange(directoryNameHash);
 
-            Parallel.ForEach(entries, entry =>
-            {
-                Result.AddRange(Calculate(entry));
-            });
+            Parallel.ForEach(entries, Calculate);
+
+            return;
         }
 
         using var fileContent = File.Open(path, FileMode.Open);
         var fileHash = MD5.HashData(fileContent);
         Result.AddRange(fileHash);
-
-        return [.. Result];
     }
 }
