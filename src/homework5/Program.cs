0c6b7917ca9378338e0ea519bd73b3d1343638a6
Ivan Pokhabov <vanek3372@gmail.com>
2024-11-10 15:17:46 +0300

    feat: add CLI of project

---
+++ src/homework5/Program.cs
@@ -0,0 +1,39 @@
+﻿using homework5.Utils;
+
+static void PrintHelp()
+{
+    Console.WriteLine("""
+
+            This is tool for run your C# test program
+
+            Usage: dotnet run {path}
+
+            """);
+}
+
+if (args.Length != 1)
+{
+    PrintHelp();
+    return;
+}
+
+var path = args[0];
+
+try
+{
+    var result = await MyNUnitUtils.RunTestsAsync(path);
+    Parallel.ForEach(result, (reports) =>
+    {
+        Console.WriteLine($"Results of test class {reports.ClassType}:");
+        Parallel.ForEach(reports.Results, (report) =>
+        {
+            var methodSucces = report.IsSuccess ? "succes" : "failed";
+            Console.WriteLine($"Method {report.MethodName} was {methodSucces} with time {report.Time}");
+        });
+    });
+}
+catch (ArgumentException e)
+{
+    Console.WriteLine(e.Message);
+    return;
+}
