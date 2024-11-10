afd7dc81b977d4f0c6ab503ba3b0de25952b537f
Ivan Pokhabov <vanek3372@gmail.com>
2024-11-10 15:16:59 +0300

    feat: add class for running test by path

---
+++ src/homework5/Utils/Utils.cs
@@ -0,0 +1,63 @@
+using System.Reflection;
+using homework5.TestRunner;
+
+namespace homework5.Utils;
+
+public static class MyNUnitUtils
+{
+    public static async Task<TestClassResult[]> RunTestsAsync(string path)
+    {
+        if (!File.Exists(path) && !Directory.Exists(path))
+        {
+            throw new ArgumentException("Invalid path.");
+        }
+
+        var assemblies = CollectAllAssemblysByPath(path);
+        var tests = await CollectTestsFromAssemblies(assemblies);
+        return await RunTestsFromClassesAsync(tests);
+    }
+
+    private static IEnumerable<Assembly> CollectAllAssemblysByPath(string path)
+        => File.Exists(path)
+        ? Path.GetExtension(path).Equals(".dll")
+            ? [Assembly.LoadFrom(path)]
+            : Array.Empty<Assembly>()
+        : Directory.GetFiles(path, "*.dll").Select(Assembly.LoadFrom);
+
+    private static async Task<MyTestRunner[]> CollectTestsFromAssemblies(IEnumerable<Assembly> assemblies)
+    {
+        var classTypes = new List<Type>();
+
+        foreach (var assembly in assemblies)
+        {
+            classTypes.AddRange(assembly.GetTypes().Where(type => type.IsClass));
+        }
+
+        var result = new MyTestRunner[classTypes.Count];
+        var tasks = classTypes.Select((type, index) => Task.Run(() =>
+        {
+            result[index] = MyTestRunner.MakeTestByClassType(type);
+        })).ToArray();
+
+        await Task.WhenAll(tasks);
+
+        return result;
+    }
+
+    private static async Task<TestClassResult[]> RunTestsFromClassesAsync(MyTestRunner[] tests)
+    {
+        var result = new TestClassResult[tests.Length];
+        var tasks = new Task[tests.Length];
+        for (var i = 0; i < tests.Length; ++i)
+        {
+            var locI = i;
+            tasks[i] = tests[i].RunTestsAsync().ContinueWith(reportTask =>
+            {
+                result[locI] = reportTask.Result;
+            });
+        }
+
+        await Task.WhenAll(tasks);
+        return result;
+    }
+}
